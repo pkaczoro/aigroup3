bba663cce575371a95e13fdfcf12635aa97de53e
pakaczor <pkaczoro@gmail.com>
2024-09-20 10:43:52 +0200

    Init

---
+++ src/test/java/org/example/model/BallotTest.java
@@ -0,0 +1,15 @@
+package org.example.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class MainTest {
+
+    @Test
+    void test() {
+        //    Main m new Main();
+
+    }
+}
